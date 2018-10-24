d95bb719463fc43322cc2fecf86993974f9184a0
Ryan Harper <rch827@gmail.com>
2018-10-23 22:33:24 -0400

    Now checks for valid email and passwords as well as verifies passwords.

--- js/login.js
+++ js/login.js
@@ -14,9 +14,13 @@
   //Get elements
   const txtEmail = document.getElementById('txtemail');
   const txtPassword = document.getElementById('txtpassword');
+  const txtPasswordVerify = document.getElementById('txtpasswordverify');
   const btnSignUp = document.getElementById('btnSignUp');
   const btnLogin = document.getElementById('btnLogin');
   const btnLogout = document.getElementById('btnLogout');
+
+
+
   //Add login event
   btnLogin.addEventListener('click', e => {
     //Get email and pass
@@ -27,18 +31,74 @@
     const promise = auth.signInWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
   });
+
+
   //Add signup event
   btnSignUp.addEventListener('click', e => {
     //Get email and pass
     //TODO: Check for real email!
     const email = txtEmail.value;
     const pass = txtPassword.value;
+    const passverify = txtPasswordVerify.value;
     const auth = firebase.auth();
+    //Verifying email address is validity
+    if(!(verifyEmail(email))){
+      alert("Please type a valid email address.");
+      txtEmail.value = '';
+      return;
+    }
+
+    /*Verifying password validity with the following parameters:
+      8 or more characters
+      Must contain at least 1 character from each of the following categories:
+      lowercase letters
+      uppercase letters
+      numbers
+      special characters !@#$%^&*(),.?":{}|<>
+    */
+    if(pass.length < 8){
+      alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
+      txtPassword.value='';
+      txtPasswordVerify.value='';
+      return;
+    }
+    else if(!(hasLowerCase(pass))){
+      alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
+      txtPassword.value='';
+      txtPasswordVerify.value='';
+      return;
+    }
+    else if(!(hasUpperCase(pass))){
+      alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
+      txtPassword.value='';
+      txtPasswordVerify.value='';
+      return;
+    }
+    else if(!(hasNumber(pass))){
+      alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
+      txtPassword.value='';
+      txtPasswordVerify.value='';
+      return;
+    }
+    else if(!(hasSpecialChar(pass))){
+      alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
+      txtPassword.value='';
+      txtPasswordVerify.value='';
+      return;
+    }
+
+    if((pass !== passverify)){
+      alert("Please make sure your passwords match.");
+      txtPasswordVerify.value='';
+      return;
+    }
     //Sign up
     const promise = auth.createUserWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
 
   });
+
+
 
   //Add a realtime listener
   firebase.auth().onAuthStateChanged(firebaseUser => {
@@ -66,8 +126,34 @@
     }
   });
 
+
+
   //Add logout event
   btnLogout.addEventListener('click', e => {
     firebase.auth().signOut();
   });
 }());
+
+
+//Helper functions that make the thing do the things
+
+function verifyEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function hasLowerCase(str) {
+    return (/[a-z]/.test(str));
+}
+
+function hasUpperCase(str) {
+    return (/[A-Z]/.test(str));
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
+
+function hasSpecialChar(str){
+  return /[!@#$%^&*(),.?":{}|<>]/g.test(str);
+}
