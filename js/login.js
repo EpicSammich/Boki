6fc9cde38b3d435141fe3a6480c3c3e715a36158
Ryan Harper <rch827@gmail.com>
2018-11-13 12:16:35 -0500

    Commented a lot of the JS code to make it workable in the future

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@
   };
   firebase.initializeApp(config);
 
-  //Get elements
+  //Get elements from HTML DOM
   const txtEmail = document.getElementById('txtemail');
   const txtPassword = document.getElementById('txtpassword');
   const txtPasswordVerify = document.getElementById('txtpasswordverify');
@@ -21,7 +21,7 @@
 
 
 
-  //Add login event
+  //Event for login button press
   if(btnLogin != null){
     btnLogin.addEventListener('click', e => {
       //Get email and pass
@@ -34,16 +34,15 @@
     });
   }
 
-  //Add signup event
+  //Event for Sign Up button press
 if(btnSignUp != null){
   btnSignUp.addEventListener('click', e => {
     //Get email and pass
-    //TODO: Check for real email!
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const passverify = txtPasswordVerify.value;
     const auth = firebase.auth();
-    //Verifying email address is validity
+    //Verifying email address is properly formatted
     if(!(verifyEmail(email))){
       alert("Please type a valid email address.");
       txtEmail.value = '';
@@ -58,43 +57,48 @@ if(btnSignUp != null){
       numbers
       special characters !@#$%^&*(),.?":{}|<>
     */
+    //Password length
     if(pass.length < 8){
       alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
       txtPassword.value='';
       txtPasswordVerify.value='';
       return;
     }
+    //Lowercase
     else if(!(hasLowerCase(pass))){
       alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
       txtPassword.value='';
       txtPasswordVerify.value='';
       return;
     }
+    //Uppercase
     else if(!(hasUpperCase(pass))){
       alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
       txtPassword.value='';
       txtPasswordVerify.value='';
       return;
     }
+    //Numbers
     else if(!(hasNumber(pass))){
       alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
       txtPassword.value='';
       txtPasswordVerify.value='';
       return;
     }
+    //Special characters
     else if(!(hasSpecialChar(pass))){
       alert("Please choose a more secure password. It should be longer than 8 characters, unique to you, and difficult for others to guess.");
       txtPassword.value='';
       txtPasswordVerify.value='';
       return;
     }
-
+    //Makes sure the password and passwordverify fields match
     if((pass !== passverify)){
       alert("Please make sure your passwords match.");
       txtPasswordVerify.value='';
       return;
     }
-    //Sign up
+    //Sign up with Firebase
     const promise = auth.createUserWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
 
@@ -102,7 +106,20 @@ if(btnSignUp != null){
 }
 
 
-  //Add a realtime listener
+//Event for Sign Up button press
+if(btnLogout != null){
+btnLogout.addEventListener('click', e => {
+  firebase.auth().signOut();
+});
+}
+}());
+
+  //Listens for a change in login/logout state.
+  //If logging in and the email address has not been verified, an email is sent
+  //so the user can verify it.
+  //If logging in and the email address has been verified, it's logged in the
+  //console that it's verified and the page redirects to the homepage.
+  //If logging out, the page redirects to the login page.
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){
       console.log(firebaseUser);
@@ -127,40 +144,34 @@ if(btnSignUp != null){
       if (window.location.href == 'http://localhost/homepage.html' ) {
         window.location.replace("http://localhost/index.html")
       }
-      //TODO: Make sure site redirects to login page without an endless loop
     }
   });
 
 
+//Helper functions
 
-  //Add logout event
-if(btnLogout != null){
-  btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-  });
-}
-}());
-
-
-//Helper functions that make the thing do the things
-
+//Verifies the email address is properly formatted
 function verifyEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
+//returns true if the string has a lowercase letter
 function hasLowerCase(str) {
     return (/[a-z]/.test(str));
 }
 
+//returns true if the string has an uppercase letter
 function hasUpperCase(str) {
     return (/[A-Z]/.test(str));
 }
 
+//returns true if the string has a number
 function hasNumber(myString) {
   return /\d/.test(myString);
 }
 
+//returns true if the string has a special character !@#$%^&*(),.?":{}|<>
 function hasSpecialChar(str){
   return /[!@#$%^&*(),.?":{}|<>]/g.test(str);
 }
