6d2730a94a0af02e3a55ede7e34487473ec8b70a
jechevarria2016 <44330167+jechevarria2016@users.noreply.github.com>
2018-12-10 15:34:34 -0500

    added voting system

--- js/viewOtherUserProfile.js
+++ js/viewOtherUserProfile.js
@@ -13,6 +13,7 @@ function getTheOtherUser(theURL) {
 
 
 
+
 theDB.orderByKey().equalTo(getTheOtherUser(window.location.href)).on("value", function(viewOtherUserProfile){
     viewOtherUserProfile.forEach(function (getTheInfo)
     {
@@ -35,3 +36,35 @@ theDB.orderByKey().equalTo(getTheOtherUser(window.location.href)).on("value", fu
              document.getElementById("thisUserTotalRatings").innerHTML = otherUsertotrate;    
     });
 });
+
+function changeRating(){
+    var arrRates = document.getElementsByName("star");
+    for(var i = 0, length = arrRates.length; i<length; i++)
+        {
+            
+            if(arrRates[i].checked)
+                {
+                    
+                    theDB.orderByKey().equalTo(getTheOtherUser(window.location.href)).once("value", function(viewOtherUserProfile){     
+viewOtherUserProfile.forEach(function (getTheInfo)
+    {
+            var otherInfo = getTheInfo.val();
+             otherUserrating = otherInfo.userrate;
+             otherUsertotrate = otherInfo.userrate_amount;
+            var otherUsertot = otherInfo.userrate_total;
+            var newUsertot =  otherUsertot + Number(arrRates[i].value);
+            var newUsertotrate = otherUsertotrate + 1;
+            var newUserrating = newUsertot/newUsertotrate;
+            console.log(arrRates[i].value);
+            console.log(newUserrating);
+            console.log(newUsertotrate);
+            console.log(newUsertot);
+            firebase.database().ref("/userProfiles/" + getTheOtherUser(window.location.href)).update({userrate: newUserrating, userrate_amount: newUsertotrate, userrate_total: newUsertot});
+    });           
+                    });
+                    
+                }
+        } 
+}
+
+
